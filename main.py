d12bf381b6bab3321fc7ba3e85d9c7f519b6142b
Niranjan-NN <129034340+Niranjan-NN@users.noreply.github.com>
2025-01-06 19:10:37 +0530

    main.py

--- main.py
+++ main.py
@@ -43,7 +43,7 @@ DB_HOST = os.getenv('DB_HOST')
 DB_USER = os.getenv('DB_USER')
 DB_PASSWORD = os.getenv('DB_PASSWORD')
 DB_NAME = os.getenv('DB_NAME')
-DB_PORT = os.getenv('DB_PORT', 3306)  # Default to 3306 if not set in .env
+DB_PORT = os.getenv('DB_PORT', 3307)  # Default to 3307 if not set in .env
 
 # Create database engine using credentials from .env
 engine = create_engine(f'mysql+pymysql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}')
